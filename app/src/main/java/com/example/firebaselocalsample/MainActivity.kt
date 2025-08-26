fde3fb5eb952a9deb5b06af7bf1f4223e0eb9f15
Denver Coneybeare <dconeybe@google.com>
2025-08-26 19:15:22 -0400

    improve ui to show setup progress

--- app/src/main/java/com/example/firebaselocalsample/MainActivity.kt
+++ app/src/main/java/com/example/firebaselocalsample/MainActivity.kt
@@ -8,16 +8,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.material3.Text
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.lifecycleScope
 import com.example.firebaselocalsample.ui.theme.FirebaseLocalSampleTheme
+import com.google.firebase.Firebase
 import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Source
+import com.google.firebase.firestore.firestore
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
+import java.util.Locale
 import kotlin.time.measureTimedValue
 
 /**
@@ -30,37 +35,44 @@ const val FETCH_COUNT = 200
 
 class MainActivity : ComponentActivity() {
 
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+        firestore = Firebase.firestore
+        firestore.useEmulator("10.0.2.2", 8080)
 
+        val documentCreateCount = mutableIntStateOf(0)
+        val documentDeleteCount = mutableIntStateOf(0)
+        val setupDone = mutableStateOf(false)
         val totalTimeOfActiveCollectionMs = mutableStateListOf<Long>()
         val totalTimeOfInactiveCollectionMs = mutableStateListOf<Long>()
 
-
         lifecycleScope.launch {
+            val activeCollectionRef = firestore.collection("users/active/docs")
+            val inactiveCollectionRef = firestore.collection("users/inactive/docs")
             if(RUN_SETUP) {
-                createAndDeleteDocuments()
+                repeat(10_000) {
+                    val documentRef = activeCollectionRef.document()
+                    documentRef.set(mapOf("hello" to "world")).await()
+                    documentCreateCount.intValue++
+                    documentRef.delete().await()
+                    documentDeleteCount.intValue++
+                }
+                setupDone.value = true
             } else {
                 repeat(FETCH_COUNT) {
                     val timedValue = measureTimedValue {
-                        FirebaseFirestore.getInstance()
-                            .collection("users/inactive/docs")
-                            .get(Source.CACHE)
-                            .await()
+                        inactiveCollectionRef.get(Source.CACHE).await()
                     }
-
                     totalTimeOfInactiveCollectionMs += timedValue.duration.inWholeMilliseconds
                 }
 
                 repeat(FETCH_COUNT) {
                     val timedValue = measureTimedValue {
-                        FirebaseFirestore.getInstance()
-                            .collection("users/active/docs")
-                            .get(Source.CACHE)
-                            .await()
+                        activeCollectionRef.get(Source.CACHE).await()
                     }
-
                     totalTimeOfActiveCollectionMs += timedValue.duration.inWholeMilliseconds
                 }
             }
@@ -73,42 +85,46 @@ class MainActivity : ComponentActivity() {
                         .systemBarsPadding()
                 ) {
                     if(RUN_SETUP) {
+                        if (setupDone.value) {
+                            Text(
+                                text = "Setup completed. " +
+                                    "Created ${documentCreateCount.intValue} documents. " +
+                                    "Deleted ${documentDeleteCount.intValue} documents.",
+                            )
+                        } else {
+                            Text(
+                                text = "Setup in progress. " +
+                                "Created ${documentCreateCount.intValue} documents. " +
+                                "Deleted ${documentDeleteCount.intValue} documents.",
+                            )
+                        }
+                    } else {
                         Text(
-                            text = "Setup complete. Disable the RUN_SETUP flag and restart the app to run the fetch tests.",
+                            text = "Average time of inactive collection: " +
+                                "${
+                                    String.format(
+                                        Locale.US,
+                                        "%.2f",
+                                        totalTimeOfInactiveCollectionMs.average()
+                                    )
+                                } ms" +
+                                "(n=${totalTimeOfInactiveCollectionMs.size})",
                         )
-                        return@Column
+                        Text(
+                            text = "Average time of active collection: " +
+                                "${
+                                    String.format(
+                                        Locale.US,
+                                        "%.2f",
+                                        totalTimeOfActiveCollectionMs.average()
+                                    )
+                                } ms " +
+                                "(n=${totalTimeOfActiveCollectionMs.size})",
+                        )
                     }
-
-                    val averageTime = totalTimeOfActiveCollectionMs.average()
-                    Text(
-                        text = "Average time of active collection: ${String.format("%.2f", averageTime)} ms",
-                    )
-
-                    val averageTimeInactive = totalTimeOfInactiveCollectionMs.average()
-                    Text(
-                        text = "Average time of inactive collection: ${String.format("%.2f", averageTimeInactive)} ms",
-                    )
                 }
             }
         }
     }
 
-    /**
-     * This function creates and deletes 10,000 documents in Firestore. Therefore the result of
-     * a query to this collection will be empty. However each document does not contain
-     * much data so this never ends up being garbage collected by Firestore.
-     */
-    private fun createAndDeleteDocuments() {
-        repeat(10_000) {
-            val ref = FirebaseFirestore.getInstance()
-                .collection("users/active/docs")
-                .document()
-
-            ref.set(
-                mapOf("hello" to "world")
-            )
-
-            ref.delete()
-        }
-    }
 }
