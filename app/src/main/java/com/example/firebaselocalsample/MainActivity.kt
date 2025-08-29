a054c0b8e61edacf71077857d39ca9c70a74b49a
Denver Coneybeare <dconeybe@google.com>
2025-08-29 00:20:51 -0400

    format code with spotless

--- app/src/main/java/com/example/firebaselocalsample/MainActivity.kt
+++ app/src/main/java/com/example/firebaselocalsample/MainActivity.kt
@@ -16,10 +16,10 @@ import com.google.firebase.Firebase
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.Source
 import com.google.firebase.firestore.firestore
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.tasks.await
 import java.util.Locale
 import kotlin.time.measureTimedValue
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 
 const val FETCH_COUNT = 200
 
@@ -30,148 +30,154 @@ const val MAX_FIRESTORE_BATCH_SIZE = 500
 
 class MainActivity : ComponentActivity() {
 
-    private lateinit var firestore: FirebaseFirestore
+  private lateinit var firestore: FirebaseFirestore
 
-    private sealed interface RunState {
-        data object NotStarted : RunState
-        data class Setup(val documentCreateCount: Int = 0, val documentDeleteCount: Int = 0) : RunState
-        data class Test(val setupResult: Setup? = null) : RunState
-        data class Done(val setupResult: Setup?) : RunState
-    }
+  private sealed interface RunState {
+    data object NotStarted : RunState
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        firestore = Firebase.firestore
-        firestore.useEmulator("10.0.2.2", 8080)
+    data class Setup(val documentCreateCount: Int = 0, val documentDeleteCount: Int = 0) : RunState
 
-        val runState = mutableStateOf<RunState>(RunState.NotStarted)
-        val totalTimeOfActiveCollectionMs = mutableStateListOf<Long>()
-        val totalTimeOfInactiveCollectionMs = mutableStateListOf<Long>()
+    data class Test(val setupResult: Setup? = null) : RunState
 
-        lifecycleScope.launch {
-            val activeCollectionRef = firestore.collection("users/active/docs")
-            val inactiveCollectionRef = firestore.collection("users/inactive/docs")
+    data class Done(val setupResult: Setup?) : RunState
+  }
 
-            val setupCompletedDocRef = firestore.document("setup/completed")
-            val setupCompleted = setupCompletedDocRef.get(Source.CACHE).runCatching { await() }.fold(onSuccess = { it.exists() }, onFailure = { false })
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    enableEdgeToEdge()
+    firestore = Firebase.firestore
+    firestore.useEmulator("10.0.2.2", 8080)
 
-            val testState: RunState.Test = if(setupCompleted) {
-                RunState.Test().also { runState.value = it }
-            } else {
-                var setupState = RunState.Setup()
-                runState.value = setupState
-                var createBatch = firestore.batch()
-                var deleteBatch = firestore.batch()
-                var batchSize = 0
+    val runState = mutableStateOf<RunState>(RunState.NotStarted)
+    val totalTimeOfActiveCollectionMs = mutableStateListOf<Long>()
+    val totalTimeOfInactiveCollectionMs = mutableStateListOf<Long>()
 
-                repeat(DOCUMENT_CREATE_AND_DELETE_COUNT) {
-                    val documentRef = activeCollectionRef.document()
-                    createBatch.set(documentRef, mapOf("foo" to it))
-                    deleteBatch.delete(documentRef)
-                    batchSize++
+    lifecycleScope.launch {
+      val activeCollectionRef = firestore.collection("users/active/docs")
+      val inactiveCollectionRef = firestore.collection("users/inactive/docs")
 
-                    if (batchSize == MAX_FIRESTORE_BATCH_SIZE
-                            || it + 1 == DOCUMENT_CREATE_AND_DELETE_COUNT) {
-                        createBatch.commit().await()
-                        setupState = setupState.run {
-                            copy(documentCreateCount=documentCreateCount+batchSize)
-                        }
-                        runState.value = setupState
+      val setupCompletedDocRef = firestore.document("setup/completed")
+      val setupCompleted =
+        setupCompletedDocRef
+          .get(Source.CACHE)
+          .runCatching { await() }
+          .fold(onSuccess = { it.exists() }, onFailure = { false })
 
-                        deleteBatch.commit().await()
-                        setupState = setupState.run {
-                            copy(documentDeleteCount=documentDeleteCount+batchSize)
-                        }
-                        runState.value = setupState
+      val testState: RunState.Test =
+        if (setupCompleted) {
+          RunState.Test().also { runState.value = it }
+        } else {
+          var setupState = RunState.Setup()
+          runState.value = setupState
+          var createBatch = firestore.batch()
+          var deleteBatch = firestore.batch()
+          var batchSize = 0
 
-                        createBatch = firestore.batch()
-                        deleteBatch = firestore.batch()
-                        batchSize = 0
-                    }
-                }
+          repeat(DOCUMENT_CREATE_AND_DELETE_COUNT) {
+            val documentRef = activeCollectionRef.document()
+            createBatch.set(documentRef, mapOf("foo" to it))
+            deleteBatch.delete(documentRef)
+            batchSize++
 
-                check(batchSize == 0) { "batchSize should be 0, but got $batchSize" }
-                setupCompletedDocRef.set(mapOf("completed" to true)).await()
-                RunState.Test(setupState).also { runState.value = it }
+            if (
+              batchSize == MAX_FIRESTORE_BATCH_SIZE || it + 1 == DOCUMENT_CREATE_AND_DELETE_COUNT
+            ) {
+              createBatch.commit().await()
+              setupState =
+                setupState.run { copy(documentCreateCount = documentCreateCount + batchSize) }
+              runState.value = setupState
+
+              deleteBatch.commit().await()
+              setupState =
+                setupState.run { copy(documentDeleteCount = documentDeleteCount + batchSize) }
+              runState.value = setupState
+
+              createBatch = firestore.batch()
+              deleteBatch = firestore.batch()
+              batchSize = 0
             }
+          }
 
-            repeat(FETCH_COUNT) {
-                val timedValue = measureTimedValue {
-                    inactiveCollectionRef.get(Source.CACHE).await()
-                }
-                totalTimeOfInactiveCollectionMs += timedValue.duration.inWholeMilliseconds
-            }
-
-            repeat(FETCH_COUNT) {
-                val timedValue = measureTimedValue {
-                    activeCollectionRef.get(Source.CACHE).await()
-                }
-                totalTimeOfActiveCollectionMs += timedValue.duration.inWholeMilliseconds
-            }
-
-            runState.value = RunState.Done(testState.setupResult)
+          check(batchSize == 0) { "batchSize should be 0, but got $batchSize" }
+          setupCompletedDocRef.set(mapOf("completed" to true)).await()
+          RunState.Test(setupState).also { runState.value = it }
         }
 
-        setContent {
-            FirebaseLocalSampleTheme {
-                Column(
-                    modifier = Modifier
-                        .systemBarsPadding()
-                ) {
-                    val setupState: RunState.Setup? = when (val state = runState.value) {
-                        RunState.NotStarted -> null
-                        is RunState.Setup -> state
-                        is RunState.Test -> state.setupResult
-                        is RunState.Done -> state.setupResult
-                    }
+      repeat(FETCH_COUNT) {
+        val timedValue = measureTimedValue { inactiveCollectionRef.get(Source.CACHE).await() }
+        totalTimeOfInactiveCollectionMs += timedValue.duration.inWholeMilliseconds
+      }
 
-                    if (setupState !== null) {
-                        when (runState.value) {
-                            RunState.NotStarted -> throw IllegalStateException("should never get here")
-                            is RunState.Setup -> Text("Setup in progress...")
-                            is RunState.Test, is RunState.Done -> Text("Setup completed.")
-                        }
-                        Text("Created ${setupState.documentCreateCount} documents.")
-                        Text("Deleted ${setupState.documentDeleteCount} documents.")
-                    }
+      repeat(FETCH_COUNT) {
+        val timedValue = measureTimedValue { activeCollectionRef.get(Source.CACHE).await() }
+        totalTimeOfActiveCollectionMs += timedValue.duration.inWholeMilliseconds
+      }
 
-                    when (runState.value) {
-                        RunState.NotStarted, is RunState.Setup -> {}
-                        is RunState.Test -> Text("Test in progress...")
-                        is RunState.Done -> Text("Test completed.")
-                    }
+      runState.value = RunState.Done(testState.setupResult)
+    }
 
-                    when (runState.value) {
-                        RunState.NotStarted, is RunState.Setup -> {}
-                        is RunState.Test, is RunState.Done -> {
-                            Text(
-                                text = "Average time of inactive collection: " +
-                                    "${
+    setContent {
+      FirebaseLocalSampleTheme {
+        Column(modifier = Modifier.systemBarsPadding()) {
+          val setupState: RunState.Setup? =
+            when (val state = runState.value) {
+              RunState.NotStarted -> null
+              is RunState.Setup -> state
+              is RunState.Test -> state.setupResult
+              is RunState.Done -> state.setupResult
+            }
+
+          if (setupState !== null) {
+            when (runState.value) {
+              RunState.NotStarted -> throw IllegalStateException("should never get here")
+              is RunState.Setup -> Text("Setup in progress...")
+              is RunState.Test,
+              is RunState.Done -> Text("Setup completed.")
+            }
+            Text("Created ${setupState.documentCreateCount} documents.")
+            Text("Deleted ${setupState.documentDeleteCount} documents.")
+          }
+
+          when (runState.value) {
+            RunState.NotStarted,
+            is RunState.Setup -> {}
+            is RunState.Test -> Text("Test in progress...")
+            is RunState.Done -> Text("Test completed.")
+          }
+
+          when (runState.value) {
+            RunState.NotStarted,
+            is RunState.Setup -> {}
+            is RunState.Test,
+            is RunState.Done -> {
+              Text(
+                text =
+                  "Average time of inactive collection: " +
+                    "${
                                         String.format(
                                             Locale.US,
                                             "%.2f",
-                                            totalTimeOfInactiveCollectionMs.average()
+                                            totalTimeOfInactiveCollectionMs.average(),
                                         )
                                     } ms" +
-                                    "(n=${totalTimeOfInactiveCollectionMs.size})",
-                            )
-                            Text(
-                                text = "Average time of active collection: " +
-                                    "${
+                    "(n=${totalTimeOfInactiveCollectionMs.size})"
+              )
+              Text(
+                text =
+                  "Average time of active collection: " +
+                    "${
                                         String.format(
                                             Locale.US,
                                             "%.2f",
-                                            totalTimeOfActiveCollectionMs.average()
+                                            totalTimeOfActiveCollectionMs.average(),
                                         )
                                     } ms " +
-                                    "(n=${totalTimeOfActiveCollectionMs.size})",
-                            )
-                        }
-                    }
-                }
+                    "(n=${totalTimeOfActiveCollectionMs.size})"
+              )
             }
+          }
         }
+      }
     }
-
+  }
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,22 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-    id("com.google.gms.google-services") version "4.4.3" apply false
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.android) apply false
+  alias(libs.plugins.kotlin.compose) apply false
+  id("com.google.gms.google-services") version "4.4.3" apply false
+  id("com.diffplug.spotless") version "7.1.0"
+}
+
+spotless {
+  val ktfmtVersion = "0.58"
+  kotlin {
+    target("**/*.kt")
+    targetExclude("build/")
+    ktfmt(ktfmtVersion).googleStyle()
+  }
+  kotlinGradle {
+    target("**/*.gradle.kts")
+    targetExclude("build/")
+    ktfmt(ktfmtVersion).googleStyle()
+  }
 }
